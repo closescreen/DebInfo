c08cfb24d77ab1dd29646a42bed178b5c61e75ac
closescreen <close.screen@gmail.com>
2017-01-20 23:19:08 +0300

    ok() , err(), infoiter()

--- src/DebInfo.jl
+++ src/DebInfo.jl
@@ -37,44 +37,65 @@ DebInfo.ProgressBar("|",1,"#",10,#1,0)
 for i in 1:100 goprogress(progressbar) end
 |########[10]#########[20]#########[30]#########[40]#########[50]#########[60]#########[70]#########[80]#########[90]#########[100]
 """
-function go(p::ProgressBar)
+function goprogress(p::ProgressBar)
  if p.cnt==0 
-  write(STDERR, p.start_msg)
+  print(STDERR, p.start_msg)
   p.cnt+=1
  else
   p.cnt+=1
   if p.cnt % p.veha == 0
-   write(STDERR, p.veha_fun(p.cnt))
+   print(STDERR, p.veha_fun(p.cnt))
   elseif p.cnt % p.step == 0
-   write(STDERR, p.step_str)
+   print(STDERR, p.step_str)
   end
  end
  p.cnt
 end
-export go
-
-"Alias for go(...) function"
-const goprogress = go
 export goprogress
 
-
 """ 
-julia> cooliter([1,2,3], x->x*2, ()->write(STDERR,"#") ) |>collect
+progressbar = ProgressBar("|",1=>"#",10=>n->"[\$n]")
+
+advans::Function = go(progressbar) # returns ()->goprogress(progressbar)
+
+for i in 1:100 advans() end
+"""
+go(p::ProgressBar)=()->goprogress(p)
+export go
+
+
+""" ok(progressbar) 
+
+ok(progressbar , "Success!") 
+
+n>5 ? ok(progressbar) : err(progressbar)
+"""
+ok(p::ProgressBar, msg::AbstractString="[ok]") = print_with_color(:green, msg)
+export ok
+
+""" err(progressbar)
+
+err(progressbar, "something wrong...") """
+err(p::ProgressBar, msg::AbstractString="[error]") = print_with_color(:red, msg)
+export err
+
+"""
+julia> infoiter( x->x*2, [1,2,3], ()->print(STDERR,"#") ) |>collect
 ###3-element Array{Int64,1}:
  2
  4
  6
 """
-function cooliter( initer, pred::Function, foreachfun::Function )
+function infoiter( pred::Function, initer, foreachfun::Function )
  ( begin rv=pred(el); foreachfun(); rv end for el in initer )
 end
-export cooliter
+export infoiter
 
-""" [1,2,3] |> cooliter(x->x*2, ()->write(STDERR,"#")) """
-cooliter( pred::Function, foreachfun::Function ) = initer->cooliter( initer, pred, foreachfun )
+""" [1,2,3] |> infoiter(x->x*2, ()->print(STDERR,"#")) """
+infoiter( pred::Function, foreachfun::Function ) = initer->infoiter( pred, initer, foreachfun )
 
 """
-julia> cooliter([1,2,3], x->x*2, "Iter A") |>collect
+julia> infoiter( x->x*2, [1,2,3], "Iter A") |>collect
 INFO: Iter A In: 1
 INFO: Iter A Out: 2
 INFO: Iter A In: 2
@@ -86,20 +107,20 @@ INFO: Iter A Out: 6
  4
  6
 """
-function cooliter( initer, pred::Function, debugprefix::AbstractString )
+function infoiter( pred::Function, initer, debugprefix::AbstractString )
  ( begin info( "$debugprefix In: $el" ); rv=pred(el); info( "$debugprefix Out: $rv"); rv end for el in initer )
 end
 
-""" [1,2,3] |> cooliter(x->x*2, "Iter A") """
-cooliter( pred::Function, debugprefix::AbstractString ) = initer->cooliter( initer, pred, debugprefix )
+""" [1,2,3] |> infoiter(x->x*2, "Iter A") """
+infoiter( pred::Function, debugprefix::AbstractString ) = initer->infoiter( pred, initer, debugprefix )
 
 
-""" cooliter([1,2,3], x->x*2) # without any debug/info """
-cooliter( initer, pred::Function) = (pred(i) for i in initer)
+""" infoiter( x->x*2, [1,2,3]) # without any debug/info """
+infoiter(  pred::Function, initer) = (pred(i) for i in initer)
 
 
-""" [1,2,3] |> cooliter(x->x*2) # without any debug/info """
-cooliter( pred::Function) = initer->cooliter( initer, pred)
+""" [1,2,3] |> infoiter(x->x*2) # without any debug/info """
+infoiter( pred::Function) = initer->infoiter( pred, initer)
 
 end # module
 
